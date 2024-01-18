31296efc8dfa17fb4d2ef258219d3616cc37e52d
Miriam Völkl <miriamvoelkl@gmail.com>
2024-01-18 15:29:52 +0100

    link cities to home page

--- pages/cities/index.js
+++ pages/cities/index.js
@@ -2,10 +2,13 @@ import { cities } from "@/lib/data";
 
 export default function Cities() {
   return (
-    <ul>
-      {cities.map((city) => (
-        <li key={city.id}>{city.name}</li>
-      ))}
-    </ul>
+    <>
+      <h1>Here are some big cities:</h1>
+      <ul>
+        {cities.map((city) => (
+          <li key={city.id}>{city.name}</li>
+        ))}
+      </ul>
+    </>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
+import Link from "next/link";
+
 export default function HomePage() {
   return (
-    <div>
-      <h1>Hello from Next.js</h1>
-    </div>
+    <>
+      <h1>🌆 Welcome to this website with some cities! 🌃</h1>
+      <Link href="/cities">Travel to the cities 🚂</Link>
+    </>
   );
 }
